ca5c66897a159d13fa34a0bdf0fc083fef3e526c
Lizzie Davison <lizzie@hubblehq.com>
2022-03-12 15:52:12 +0000

    Basic create-react-app electron setup
    No proper builds thats next but does a basic setup so yayyyyyy

---
+++ public/electron.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev');
+const path = require('path')
+
+try {
+    require('electron-reloader')(module)
+} catch (_) {}
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+        }
+    })
+
+    // and load the index.html of the app.
+    // win.loadFile("index.html");
+    win.loadURL(
+      isDev
+        ? 'http://localhost:3000'
+        : `file://${path.join(__dirname, '../build/index.html')}`
+    );
+    // Open the DevTools.
+    if (isDev) {
+      win.webContents.openDevTools({ mode: 'detach' });
+    }
+}
+
+app.whenReady().then(createWindow);
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
